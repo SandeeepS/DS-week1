1feae98ca885e52ffdbf44b1d998196c4ee54539
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-02-20 11:56:06 +0530

    testing code

---
+++ testlink.js
@@ -0,0 +1,92 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty(){
+        return this.size === 0 
+    }
+    getSize(){
+        return this.size
+    }
+ 
+    //prepend
+    prepend(value){
+        const node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node
+        }else{
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++
+    }
+
+    //append
+    append(value){
+        const node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            let prev = this.head
+            while(prev.next){
+                prev = prev.next
+            }
+            prev.next = node;
+        }
+        this.size++
+    }
+
+    //insert
+    insert(value,index){
+        if(index < 0 || index > this.size){
+            return 
+        }
+        if(index == 0){
+            this.prepend(value);
+        }else{
+            const node = new Node(value);
+            let prev = this.head;
+            for(let i = 0 ; i < index-1 ; i++){
+                prev = prev.next
+            }
+            node.next = prev.next;
+            prev.next = node;
+            this.size++;
+        }
+    }
+
+    print(){
+        if(this.isEmpty()){
+            console.log("the list is empty");
+        }else{
+            let curr = this.head;
+            let listVAlues = ''
+            while(curr){
+                listVAlues += `${curr.value} `
+                curr = curr.next;
+            }
+            console.log(listVAlues);
+        }
+    }
+
+}
+
+const list = new LinkedList();
+list.print()
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+list.append(90);
+list.append(80);
+list.print();
+list.insert(55,3);
+list.print();
